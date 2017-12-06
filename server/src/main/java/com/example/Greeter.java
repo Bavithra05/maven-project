4ed3f625bfc1949abba81c07c750d3d5317f035a
bavithra <bavithraa.d@gmail.com>
2017-12-06 20:12:08 +0530

    added new line

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,11 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-  
+
 /**
 *someone
-*/
-  
+*/  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
